5ca57230aeb944e6da8c28e10ff78a1cc3555d8d
wutongqiu <136473235@qq.com>
2015-07-15 10:39:12 +0800

    Add lob.write/lob.close

--- test/collection_lob.test.js
+++ test/collection_lob.test.js
@@ -1,3 +1,21 @@
+/**
+ *      Copyright (C) 2015 SequoiaDB Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+'use strict';
+
 var expect = require('expect.js');
 var common = require('./common');
 var Collection = require('../lib/collection');
@@ -8,7 +26,7 @@ describe('Collection Lob', function () {
   var collection;
 
   var spaceName = 'foo7';
-  var collectionName = "bar8";
+  var collectionName = "bar9";
 
   before(function (done) {
     this.timeout(8000);
@@ -65,5 +83,48 @@ describe('Collection Lob', function () {
     });
   });
 
+  it('getLobs should ok with item', function (done) {
+    collection.getLobs(function (err, cursor) {
+      expect(err).not.to.be.ok();
+      expect(cursor).to.be.ok();
+      cursor.current(function (err, item) {
+        expect(err).not.to.be.ok();
+        expect(item).to.be.ok();
+        done();
+      });
+    });
+  });
 
+  it('Lob.write should ok', function (done) {
+    lob.write(new Buffer("1234567890"), function (err) {
+      expect(err).not.to.be.ok();
+      done();
+    });
+  });
+
+  it('Lob.close should ok', function (done) {
+    lob.close(function (err) {
+      expect(err).not.to.be.ok();
+      done();
+    });
+  });
+
+  it('removeLob should ok', function (done) {
+    collection.removeLob(lob.id, function (err) {
+      expect(err).not.to.be.ok();
+      done();
+    });
+  });
+
+  it('getLobs should ok with empty', function (done) {
+    collection.getLobs(function (err, cursor) {
+      expect(err).not.to.be.ok();
+      expect(cursor).to.be.ok();
+      cursor.current(function (err, item) {
+        expect(err).not.to.be.ok();
+        expect(item).to.be(null);
+        done();
+      });
+    });
+  });
 });
