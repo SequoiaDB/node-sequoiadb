22d3aafdc93fa7de6ab2a74f39ab571adc1957cb
wutongqiu <136473235@qq.com>
2015-07-16 23:40:22 +0800

    Add createReplicaCataGroup method

--- test/replica_group.test.js
+++ test/replica_group.test.js
@@ -59,6 +59,18 @@ describe('Collection DML', function () {
     });
   });
 
+  it('createReplicaCataGroup should ok', function (done) {
+    this.timeout(8000);
+    var host = '1426595184.dbaas.sequoialab.net';
+    var port = 12167;
+    var dbpath = '/opt/sequoiadb/database/data/11832';
+    conn.createReplicaCataGroup(host, port, dbpath, null, function (err) {
+      expect(err).to.be.ok();
+      expect(err.message).to.be("Unable to create new catalog when there's already one exists");
+      done();
+    });
+  });
+
   it('activateReplicaGroup should ok', function (done) {
     conn.activateReplicaGroup('group5', function (err, group) {
       expect(err).not.to.be.ok();
